e135d85537336cd65b45484150e37a50ee60605e
mrambler90 <mickyre791@gmail.com>
2015-11-27 16:13:19 +0100

    Serial TX/RX completed. Radio TX/RX development

---
+++ LaboratoryExam/RadioChat.h
@@ -0,0 +1,15 @@
+#ifndef RADIO_CHAT_H
+#define RADIO_CHAT_H
+
+typedef nx_struct radio_chat {
+	nx_uint16_t nodeID;
+	nx_uint8_t message[60];
+} radio_chat_t;
+
+enum {
+	AM_BLINKTORADIO = 6,
+	TIMER_PERIOD_MILLI = 250,
+	AM_RADIO_MSG_ID = 0x99
+};
+
+#endif
